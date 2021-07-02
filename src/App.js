88413f4991a23cd2107fa87418d24af99563791c
SR Seetharam <srseetharam1999@gmail.com>
2021-07-02 14:44:43 +0530

    everything new to changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './ZD.jpg';
 import './App.css';
 
 function App() {
@@ -6,18 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+          <p style={{color:"#712C18"}}>
+            This Page is under Proccessing...
+          </p>
+        </div>
+        </header>
     </div>
   );
 }
